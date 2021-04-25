fa6afe62e8761f0a0cbd74a9f8485819278ff091
Jaleel Adesina <jtutoff@gmail.com>
2021-04-25 06:28:26 +0100

    added discovery-server

---
+++ discovery-server/src/main/java/com/japharr/grpc/discovery/Discovery.java
@@ -0,0 +1,15 @@
+package com.japharr.grpc.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Discovery {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Discovery.class, args);
+	}
+
+}
---
+++ discovery-server/src/test/java/com/japharr/grpc/discovery/DiscoveryTests.java
@@ -0,0 +1,13 @@
+package com.japharr.grpc.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
